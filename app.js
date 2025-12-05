8d3204bebb0370e60c01db31ad38b97f7411cd12
rahma aulia <rahma484@gmail.com>
2025-12-06 06:42:29 +0700

    Initial commit with full project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const userRoute = require('./routers/userRoute')
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use('/api/books', userRoute)
+
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}/api/books`);
+})
---
+++ controllers/userController.js
@@ -0,0 +1,84 @@
+const userModel = require('../models/userModel')
+
+const getAllBooks = async(req,res)=>{
+    try{
+        const books =await userModel.getAllBooks()
+        res.json(books)
+    }
+    catch (error){
+        console.log("ERROR GET ALL BOOK:", error);
+        res.status(500).json(
+            {
+                message : "Error Get All Book",
+                status : 500
+            }
+        )
+    }
+}
+
+const getBookByCode = async(req, res)=>{
+    try{
+        const book = await userModel.getBookByCode(req.params.code);
+
+        if (!book){
+            return res.status(400).json(
+            {
+                message : "Data not found"
+            })
+        }
+
+    res.status(200).json(book);
+    }
+    catch (error){
+        res.status(500).json({message: error})
+    }
+}
+const addBook = async(req, res)=>{
+   const {kode, judul, pengarang, penerbit} = req.body
+   let iskode = true
+   let isjudul = true
+   let msg = ""
+   if (!kode){
+        msg = msg + "Kode buku wajib diisi\n"
+        iskode=false
+   }
+   if (!judul){
+        msg = msg + "Judul buku wajib diisi\n"
+        isjudul=false
+   }
+   if(iskode && isjudul){
+        try{
+            const affected = await userModel.addBook(req.body)
+            if(affected==1){
+                res.status(201).json({
+                    message: "Insert succesfull",
+                    data: {...req.body}
+                })
+            }
+
+        }catch (error){
+            res.status(400).json({
+                message: error
+            })
+        }
+    }
+    else{
+        res.status(400).json({msg:msg})
+    }
+}
+const delBook = async (req, res)=>{
+    try{
+        const result = await userModel.delBook(req.params.code)
+        if(result==1){
+            res.status(200).json({msg: "Delete is succesfull"})
+        }
+        else{
+            res.status(400).json({msg: "Failed"})
+        }
+
+    } catch (error){
+        res.status(400).json({msg: error})
+    }
+}
+
+module.exports = {getAllBooks, getBookByCode, addBook, delBook}
