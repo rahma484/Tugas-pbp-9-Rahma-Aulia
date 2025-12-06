abc5a6db064ce63cddbfdcc83f31d1c594897979
rahma484 <rahmaauliayt484@gmail.com>
2025-12-06 19:24:54 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ const pool = mysql.createPool({
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME,
-    port: 4306 
 }) 
 
 const poolPromise = pool.promise()
